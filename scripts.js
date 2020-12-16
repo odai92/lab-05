4d68f7ce6422da5a1a82d4fc6f64a48978857385
Odai Qudah <odai92@live.com>
2020-12-16 21:15:22 +0200

    update

--- scripts.js
+++ scripts.js
@@ -41,3 +41,47 @@ while (x < 10) {
     console. log(x)
     
 }
+
+
+
+
+let getItem = function(){
+    let userInput = prompt('what kind of pet u need?');
+    let item = '';
+    
+    while(userInput !== 'cat' && userInput !== "dog") {
+        userInput = prompt('please choose cat or dog!'); 
+    }
+
+    if(userInput === 'cat'){
+        item =  < img src= 'images/cat.png' />
+    } else if (userInput === 'dog'){
+        item = < img src='images/cat.png' /> 
+           }
+
+           return item;
+
+}
+
+let howMany = function(){
+    let count = prompt('How Many Pets u need?');
+
+    while (count === '' || isNaN(count)) {
+        count = prompt('How Many Pets u need?');
+        return count;
+    }
+}
+
+
+let showOrder = function(){
+    let item = getItem();
+   let total = howMany();
+   let result = '';
+
+    for(let i = 0; i < total; i++) {
+      result = result + '<p>Model #' + i + '' + item + '</p>'
+    }
+
+// document.write(item);
+    return result();
+}
